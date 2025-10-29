e9c5053de19c2e9a8eef63cdbc36c43ecd5bf5d0
Jesus Casas <javiercasas740@gmail.com>
2025-10-29 00:24:50 -0700

    feat: adding 'created_at' and 'updated_at' columns

--- backend/models.py
+++ backend/models.py
@@ -12,4 +12,6 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     phonenumber = Column(Integer, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
 
