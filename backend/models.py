3d158bf53d08e3afb31f9c0fda563470e083f9c3
Jesus Casas <javiercasas740@gmail.com>
2025-10-28 23:51:54 -0700

    feat: updating user model in database to include new columns 'firstname' 'lastname'

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,4 @@
+from sqlite3 import Time
 from sqlalchemy import Column, Integer, String
 from database import Base
 
@@ -5,8 +6,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
     username = Column(String, unique=True, index=True, nullable=False)
+    firstname = Column(String, nullable=False)
+    lastname = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable = False)
-
+    phonenumber = Column(Integer, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
