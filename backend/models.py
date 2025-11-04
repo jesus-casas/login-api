5cf65b65bd40a702e532f86d540bb9a4e8a75595
Jesus Casas <javiercasas740@gmail.com>
2025-11-03 23:31:42 -0800

    fix: changed phone number to string as the usual length of a phone number would exceed postgresql's integer max num

--- backend/models.py
+++ backend/models.py
@@ -10,8 +10,8 @@ class User(Base):
     firstname = Column(String, nullable=False)
     lastname = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
-    phonenumber = Column(Integer, unique=True, index=True, nullable=False)
+    phonenumber = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
-
+    
