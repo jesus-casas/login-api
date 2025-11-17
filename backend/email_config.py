a550311c2942a6bc88c7713e2b01bf407294981f
Jesus Casas <javiercasas740@gmail.com>
2025-11-17 00:43:14 -0800

    feat: updating email config file to use pydantic settings which is the best practices for handling the .env file

--- backend/email_config.py
+++ backend/email_config.py
@@ -1,16 +1,18 @@
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 from pydantic import EmailStr
+from settings import settings
 
 conf = ConnectionConfig(
-    MAIL_USERNAME="",
-    MAIL_PASSWORD="",
-    MAIL_FROM="",
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
+    MAIL_USERNAME=settings.MAIL_USERNAME,
+    MAIL_PASSWORD=settings.MAIL_PASSWORD,
+    MAIL_FROM=settings.MAIL_FROM,
+    MAIL_PORT=settings.MAIL_PORT,
+    MAIL_SERVER=settings.MAIL_SERVER,
+    MAIL_STARTTLS=settings.MAIL_STARTTLS,
+    MAIL_SSL_TLS=settings.MAIL_SSL_TLS,
     USE_CREDENTIALS=True
 )
+
 
 async def send_email(subject: str, email_to: EmailStr, body: str):
     message = MessageSchema(
